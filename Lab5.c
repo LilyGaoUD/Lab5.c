ea193b0dfa6318ab16a511de7bd30465daded6de
LilyGaoUD <127171294+LilyGaoUD@users.noreply.github.com>
2023-03-10 23:02:52 -0500

    Create Lab5.c

---
+++ Lab5.c
@@ -0,0 +1,21 @@
+#include <studei.h>
+
+#define MAX_NUMBER 100
+int main(int argc, char** argv){
+        char
+remainders[MAX_NUMBER];
+        int number;
+        
+        int i=0;
+        scanf("%d",&number);
+        while(number != 0){
+                remainders[i] = number % 2;
+                number=number/2;
+                         i++;
+                }
+                for(int x=i-1; x >= 0; x--){
+printf("%d", remainders[x]);
+        }
+        printf("\n");
+        return 0;
+}
